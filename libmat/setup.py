63c8a1ba97e4f0ef1f0501e02d41a0f0f4df46c4
font71 <paolo.fontana@fmach.it>
2022-11-07 10:33:01 +0100

    Update setup.py
    distutils substituted by setuptools

--- libmat/setup.py
+++ libmat/setup.py
@@ -1,4 +1,7 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize('libdist.pyx', gdb_debug=True), compiler_directives={'language_level' : "3"})
+
+# setup(ext_modules=cythonize('libdist.pyx', gdb_debug=True), compiler_directives={'language_level' : "3"})
+setup(ext_modules=cythonize('libdist.pyx', gdb_debug=True))
